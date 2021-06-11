6c091959549cb6813e56672b7edab26047ebdee1
Maxim Mosin <me@lznet.dev>
2021-06-11 17:11:28 +0900

    Version 0.1.0: Add base DB support

--- README.md
+++ README.md
@@ -9,6 +9,14 @@
 `TGBOT_TEST_TOKEN` (str) - Тестовый токен бота. Стандарт: `"UNKNOWN"`.  
 `TGBOT_ADMIN_ID` (int) - Telegram userID администратора. Стандарт: `0`.  
 `TGBOT_DEFAULT_LANG` (str) - Стандартный язык для бота. Стандарт: `"ru"`.  
+`DB_HOST` (str) - Хост БД. Стандарт: `"localhost"`.  
+`DB_PORT` (int) - Порт БД. Стандарт: `5432`.  
+`DB_USERNAME` (str) - Пользователь БД. Стандарт: `"UNKNOWN"`.  
+`DB_PASSWORD` (str) - Пароль пользователя БД. Стандарт: `"UNKNOWN"`.  
+`DB_BASE_NAME` (str) - Имя базы в БД. Стандарт: `"UNKNOWN"`.  
+`DB_SSL` (str) - Режим SSL. Стандарт: `"prefer"`.  
+`DB_SSL_ROOTCERT` (PathLike) - Путь к корневому сертификату БД. Стандарт: `""`.
 
 ## Доступные языки
  * `ru` - Русский.
+ * `en` - Английский.
--- config.py
+++ config.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 
 BOT_NAME = 'F_60893'
-VERSION = '0.0.1'
+VERSION = '0.1.0'
 USR_TEMP = {
     'uid': 0,
     'registered': False,
@@ -30,16 +30,30 @@ class Configuration:
         TEST_TOKEN (str) - Тестовый токен бота. Стандарт: "UNKNOWN".
         ADMIN_ID (int) - Telegram userID администратора. Стандарт: 0.
         DEFAULT_LANG (str) - Стандартный язык для бота. Стандарт: "ru".
+        DB_HOST (str) - Хост БД. Стандарт: "localhost".
+        DB_PORT (int) - Порт БД. Стандарт: 5432.
+        DB_USERNAME (str) - Пользователь БД. Стандарт: "UNKNOWN".
+        DB_PASSWORD (str) - Пароль пользователя БД. Стандарт: "UNKNOWN".
+        DB_BASE_NAME (str) - Имя базы в БД. Стандарт: "UNKNOWN".
+        DB_SSL (str) - Режим SSL. Стандарт: "prefer".
+        DB_SSL_ROOTCERT (PathLike) - Путь к корневому сертификату БД. Стандарт: "". 
     '''
     def __init__(self) -> None:
-        self.DEBUG = bool(environ.get(f'TGBOT_DEBUG', False))
+        self.DEBUG = bool(environ.get('TGBOT_DEBUG', False))
         self.DEV = bool(environ.get('TGBOT_DEV', False))
-        self.TOKEN = str(environ.get(f'TGBOT_TOKEN', 'UNKNOWN'))
-        self.TEST_TOKEN = str(environ.get(f'TGBOT_TEST_TOKEN', 'UNKNOWN'))
+        self.TOKEN = str(environ.get('TGBOT_TOKEN', 'UNKNOWN'))
+        self.TEST_TOKEN = str(environ.get('TGBOT_TEST_TOKEN', 'UNKNOWN'))
         self.VERSION = VERSION
         self.NAME = BOT_NAME
-        self.ADMIN_ID = int(environ.get(f'TGBOT_ADMIN_ID', 0))
-        self.DEFAULT_LANG = str(environ.get(f'TGBOT_DEFAULT_LANG', 'ru'))
+        self.ADMIN_ID = int(environ.get('TGBOT_ADMIN_ID', 0))
+        self.DEFAULT_LANG = str(environ.get('TGBOT_DEFAULT_LANG', 'ru'))
+        self.DB_HOST = environ.get('TGBOT_DB_HOST', 'localhost')
+        self.DB_PORT = environ.get('TGBOT_DB_PORT', 5432)
+        self.DB_USERNAME = environ.get('TGBOT_DB_USERNAME', 'UNKNOWN')
+        self.DB_PASSWORD = environ.get('TGBOT_DB_PASSWORD', 'UNKNOWN')
+        self.DB_BASE_NAME = environ.get('TGBOT_DB_BASE_NAME', 'UNKNOWN')
+        self.DB_SSL = environ.get('TGBOT_DB_SSL', 'prefer')
+        self.DB_SSL_ROOTCERT = environ.get('TGBOT_DB_SSL_ROOTCERT', '')
 
     def getToken(self) -> str:
         '''Возвращает Telegram Bot Token основываясь на состоянии self.debug.
