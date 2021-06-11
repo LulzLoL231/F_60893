9a3a19ef36569b4fb1c1e68d85ea2aafc7726296
Maxim Mosin <me@lznet.dev>
2021-06-11 23:07:54 +0900

    Доделал базу для БД

--- runtime.py
+++ runtime.py
@@ -3,16 +3,17 @@
 #  F_60893 - runtime vars & funcs.
 #  Created by LulzLoL231 at 10/6/21
 #
+from typing import Any, Dict
+from logging import getLogger
 from asyncio import get_event_loop
-from typing import Any, Dict, List
 
 from aiogram import Bot, Dispatcher
-from aiogram.dispatcher import storage
 from aiogram.types import BotCommand
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 import i18n
 from config import config
+from database import Database
 
 
 cmds = [
@@ -21,7 +22,7 @@ cmds = [
     BotCommand('version', 'Bot version')
 ]
 
-
+getLogger(config.NAME).info(f'Telegram Bot {config.NAME} v{config.VERSION} Loading...')
 loop = get_event_loop()
 bot = Dispatcher(
     Bot(config.getToken(), loop, parse_mode='HTML'),
@@ -29,7 +30,9 @@ bot = Dispatcher(
     storage=MemoryStorage()
 )
 langs = i18n.Languages()
-db: Dict[str, Dict[int, Dict[str, Any]]] = {
+tempDB: Dict[str, Dict[int, Dict[str, Any]]] = {
     'users': {}
 }
+db = Database()
+loop.run_until_complete(db.check_database())
 loop.run_until_complete(bot.bot.set_my_commands(cmds))
--- start.py
+++ start.py
@@ -3,8 +3,6 @@
 #  F_60893 - startup script.
 #  Created by LulzLoL231 at 10/6/21
 #
-from logging import getLogger
-
 from aiogram.utils.executor import start_polling
 
 
@@ -14,7 +12,4 @@ from config import config
 
 
 if __name__ == '__main__':
-    getLogger(config.NAME).info(
-        f'Telegram Bot {config.NAME} v{config.VERSION}'
-    )
     start_polling(bot)
